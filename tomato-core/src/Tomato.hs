4e0dfad894607e81315279c6a98dc373196500cd
jxv <joevargas@hush.com>
2014-06-28 09:11:27 -0700

    core: refactoring

--- tomato-core/src/Tomato.hs
+++ tomato-core/src/Tomato.hs
@@ -116,41 +116,23 @@ sessionStep time_limit =
 
 sessionNudgeTimer :: Int -> SessionM ()
 sessionNudgeTimer iter_limit =
-  do iter <- use iteration
-     if | iter > iter_limit  -> do iteration .= 0
-                                   timer .= NotStarted
-        | iter == iter_limit -> do interval .= LongBreak
-                                   timer .= NotStarted
-        | otherwise          -> sessionNudgeTimerNow iter_limit
-
-
-sessionNudgeTimerNow :: Int -> SessionM ()
-sessionNudgeTimerNow iter_limit =
   do t <- use timer
      time <- io getCurrentTime
      case t of
        NotStarted  -> timer .= Running 0 time
        Paused{..}  -> timer .= Running pausedSeconds time
        Running{..} -> timer .= Paused runningSeconds
-       Finished    -> do int <- use interval
+       Finished    -> do timer .= NotStarted
+                         int <- use interval
                          case int of
                            Pomodoro   -> do iteration %= (+1)
                                             iter <- use iteration
                                             interval .= if iter < iter_limit
                                                            then ShortBreak
                                                            else LongBreak
-                                            timer .= NotStarted
-                           ShortBreak -> do interval .= Pomodoro
-                                            timer .= NotStarted
-                           LongBreak  -> return ()
-
-
-stepSession :: Session -> Double -> IO Session
-stepSession s n = execStateT (sessionStep n) s
-
-
-nudgeSessionTimer :: Session -> Int -> IO Session
-nudgeSessionTimer s n = execStateT (sessionNudgeTimer n) s
+                           ShortBreak -> interval .= Pomodoro
+                           LongBreak  -> do iteration .= 0
+                                            interval .= Pomodoro
 
 
 stepTomato :: Tomato -> IO Tomato
@@ -160,25 +142,24 @@ stepTomato tom =
                         Pomodoro   -> grp^._1
                         ShortBreak -> grp^._2
                         LongBreak  -> grp^._3
-     sess <- stepSession (tom^.session) (time_limit * 60)
+     sess <- execStateT (sessionStep (time_limit * 60)) (tom^.session)
      return (set session sess tom)
 
 
 nudgeTomatoTimer :: Tomato -> IO Tomato
 nudgeTomatoTimer tom =
-  do sess <- nudgeSessionTimer (tom^.session) (tom^.iterations)
+  do sess <- execStateT (sessionNudgeTimer (tom^.iterations)) (tom^.session)
      return (set session sess tom)
 
 
 nudger :: Tomato -> Nudger
-nudger tom =
-  case (tom^.session^.timer) of
-    NotStarted -> Start
-    Running{}  -> Pause
-    Paused{}   -> Resume
-    Finished   -> if tom^.session^.interval == LongBreak
-                     then Restart
-                     else Next
+nudger tom = case (tom^.session^.timer) of
+  NotStarted -> Start
+  Running{}  -> Pause
+  Paused{}   -> Resume
+  Finished   -> if tom^.session^.interval == LongBreak
+                   then Restart
+                   else Next
 
 
 io :: MonadIO m => IO a -> m a
